9151672973348331e98be89f117b6e573957d8d5
monaeman <mona.eman2018@gmail.com>
2023-08-08 13:22:04 -0400

    added css to the link and the crossword game

--- src/Game.jsx
+++ src/Game.jsx
@@ -4,19 +4,31 @@ const Game = () => {
   const gameImageUrl = 'https://a.storyblok.com/f/239645/600x304/4a2ba3c8e9/daily-crossword.png';
   const gameUrl = 'https://www.dictionary.com/e/crossword/';
 
+  const containerStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center', // Aligns content to the left
+  };
+
+  const gameStyle = {
+    marginRight: '20px', // Adds spacing between game and link
+  };
+
   return (
-    <div>
-      <a href={gameUrl} target="_blank" rel="noopener noreferrer">
-        <h2>Crossword</h2>
-        <img src={gameImageUrl} alt="Game" style={{ width: "200px", height: "200px", marginTop: "20px" }} />
-      </a>
-<h3> Get that job </h3>     
-<a href="https://www.thesaurus.com/e/writing/how-do-i-write-a-resume/" target="_blank" rel="noopener noreferrer">
-        Your Guide To Writing A Strong Resume
-      </a>
-
-
-      
+    <div style={containerStyle}>
+      <div style={gameStyle}>
+        <a href={gameUrl} target="_blank" rel="noopener noreferrer">
+          <h2>Crossword</h2>
+          <img src={gameImageUrl} alt="Game" style={{ width: "200px", height: "200px", marginTop: "20px" }} />
+        </a>
+      </div>
+      <div>
+        <h3>__________________________________________________________________</h3>
+        <h2 style={{ color: "dark gray", textAlign: "left" , fontSize: "24px"}} >Get that job</h2>
+        <a href="https://www.thesaurus.com/e/writing/how-do-i-write-a-resume/" target="_blank" rel="noopener noreferrer">
+          Your Guide To Writing A Strong Resume
+        </a>
+      </div>
     </div>
   );
 };
