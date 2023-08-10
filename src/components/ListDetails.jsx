74731f47de4799e6d5bb0e3de74344c436ced39a
monaeman <mona.eman2018@gmail.com>
2023-08-10 12:08:08 -0400

    deleted unnessecary codes

--- src/components/ListDetails.jsx
+++ src/components/ListDetails.jsx
@@ -2,18 +2,6 @@ import React from "react";
 
 function ListDetails({ result }) {
   const loaded = () => {
-    // Check if 'result' is defined
-    //if (!result) {
-    //return <div>No data available</div>;
-    //}
-
-    // Destructure properties from the 'result' object
-    //const { word, definition } = result;
-
-    // Check if 'result' is defined
-    //if (!result) {
-    // return <div>No data available</div>;
-    //}
     console.log(result);
     return (
       <div className="card mt-20">
@@ -23,10 +11,6 @@ function ListDetails({ result }) {
             <h3>Definition:</h3>
             <p>{result.definition}</p>
           </div>
-          {/*<div>
-          <h3>Example:</h3>
-          <p>{result.example}</p>
-  </div>*/}
         </div>
       </div>
     );
